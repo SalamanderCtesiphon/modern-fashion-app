95eb2c9fd2070e7dbf755cd580d3d4f956a65b78
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 12:45:08 -0500

    added a sign in form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { auth } from "../config/firebase"
+import { createUserWithEmailAndPassword, signOut } from 'firebase/auth'
 
 function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+   
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password)
+    } catch (err) {
+      console.err(err)
+    }
+  }
+
+  const logout = async () => {
+    try {
+      await signOut(auth)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <div><h1>Sign IN</h1></div>
+    <div className='page'>
+      <div className='card'>
+        <input 
+          placeholder='Email...' 
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input 
+          placeholder='Password...' 
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={signIn}>Sign In</button>
+
+        <button onClick={logout} >Logout</button>
+      </div>
+    </div>
   )
 }
 
