562478356a351ca227657aff905a5d29f1a51cb3
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 11:32:09 -0500

    created the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className="left-container">
+        <div className="icon-holder">ICON</div>
+        <h3 className="title">MODERN FASHION</h3>
+      </div>
+      <div className="right-container">
+        <ul className="link-list">
+          <li className="link">TOPS</li>
+          <li className="link">PANTS</li>
+          <li className="link">SHOES</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
