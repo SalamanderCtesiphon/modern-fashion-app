be39f9e9b4a7af903e3980db1e616f62fedd5f4f
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 18:50:50 -0500

    decided to host the inventory assests on the client

--- src/pages/Pants.jsx
+++ src/pages/Pants.jsx
@@ -1,43 +1,8 @@
-import React, {useEffect, useState} from 'react'
-import { db } from "../config/firebase"
-import { collection, getDocs } from 'firebase/firestore'
+import React from 'react'
 
 function Pants() {
-  const [prodcutList, setProdcutList] = useState([])
-
-  const productCollectionRef = collection(db, "inventory")
-
-  useEffect(() => {
-    const getProductList = async () => {
-      try {
-        const data = await getDocs(productCollectionRef)
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id
-        }))
-        setProdcutList(filteredData)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    getProductList()
-  }, [])
-
-  const filteredProducts = prodcutList.filter((product) => {
-    return product.category === 'pants'
-  })
   return (
-    <div>
-      <h1>Pants</h1>
-      <ul>
-        {filteredProducts.map((product) => (
-          <li key={product.id}>
-            <h3>{product.title}</h3>
-            <p>{product.price}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <div>Pants</div>
   )
 }
 
