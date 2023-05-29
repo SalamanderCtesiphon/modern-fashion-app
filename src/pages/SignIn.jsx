d38f44d1f67f0618469f44203826aaa90136ad92
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 13:28:04 -0500

    implementing authorization

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,45 +1,19 @@
-import React, { useState } from 'react'
-import { auth } from "../config/firebase"
-import { createUserWithEmailAndPassword, signOut } from 'firebase/auth'
+import React from 'react'
+
 
 function SignIn() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-   
-  const signIn = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password)
-      console.log('success')
-    } catch (err) {
-      console.log(err)
-    }
-  }
 
-  const logout = async () => {
-    try {
-      await signOut(auth)
-      console.log('signed out')
-    } catch (err) {
-      console.log(err)
-    }
-  }
+
 
   return (
     <div className='page'>
       <div className='card'>
-        <input 
-          placeholder='Email...' 
-          type='email'
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input 
-          placeholder='Password...' 
-          type='password'
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={signIn}>Sign In</button>
-
-        <button onClick={logout} >Logout</button>
+        <h3>Welcome to our site!</h3>
+        <a href="signinform">Sign In</a>
+        <p>or</p>
+        <a href="home">Continues as a guest</a>
+        <p>Not a member?</p>
+        <a href="">Sign Up!</a>
       </div>
     </div>
   )
