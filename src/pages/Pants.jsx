e3e0fdeed6613bf329416ae8374c6a23188c5a34
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 19:25:07 -0500

    populated the page with inventory

--- src/pages/Pants.jsx
+++ src/pages/Pants.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { productsArray } from '../utility/inventory'
+
 
 function Pants() {
+  const tempArray= productsArray.filter((product) => {
+    return product.category === "pants"
+  })
+
   return (
-    <div>Pants</div>
+    <div className='shop-page'>
+      <h1>Shoes</h1>
+      <div className="product-display">
+        {tempArray.map((product, idx) => (
+          <div className="product-card" key={product.id}>
+            <h3>{product.title}</h3>
+            <img src={product.image} alt="product image" />
+            <p>price: {product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
