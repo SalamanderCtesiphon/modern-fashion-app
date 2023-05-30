32f6ddbf3a498a04ee3a6ff56f85532015a8e938
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-30 11:08:08 -0500

    created a server for the app

---
+++ server.js
@@ -0,0 +1,4 @@
+// coffee: price_1NDUqHALqsWPo0rqaedrsnqV
+// FIRESTORE_API_KEY=sk_test_51NDHdfALqsWPo0rqTYarcWhGze7u5MV3CUfzuJ5XqHacDEVV5FKuMrSjPLv9XfFzroB1DTC6H7mfxVmAKBgA0Dz8006c2RGJAB
+// sunglasses: price_1NDUryALqsWPo0rqBrjlBt4k
+// camera: price_1NDUsuALqsWPo0rqXmagBcXk
