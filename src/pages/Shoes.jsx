1a69cfbc3ac8b2e69cf1d416bfe7d5fa03e136a6
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 19:25:51 -0500

    pupulated the category pages with inventory

--- src/pages/Shoes.jsx
+++ src/pages/Shoes.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { productsArray } from '../utility/inventory'
 
 function Shoes() {
+  const tempArray= productsArray.filter((product) => {
+    return product.category === "footware"
+  })
   return (
-    <div>Shoes</div>
+    <div className='shop-page'>
+      <h1>Shoes</h1>
+      <div className="product-display">
+        {tempArray.map((product, idx) => (
+          <div className="product-card" key={product.id}>
+            <h3>{product.title}</h3>
+            <img src={product.image} alt="product image" />
+            <p>price: {product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
