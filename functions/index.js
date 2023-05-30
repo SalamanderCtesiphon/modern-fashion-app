87ee591d3c5524d4639c75a664b7c38924c9bd3a
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-30 10:41:05 -0500

    tried to set up a stripes payment processor using firebase functions

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,31 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.createStripeCheckout = functions.https.onCall(async (data, context) => {
+  // stripe init
+  const stripe = require("stripe")(fucntions.config().stripe.secret_key);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: "http:..localhost:5500/success",
+    cancel_url: "http://localhost:5500/cancel",
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: "usd",
+          unit_amount: (100) * 100,
+          product_data: {
+            name: "New Camera",
+          },
+
+        },
+      },
+    ],
+  });
+
+  return {
+    id: session.id,
+  };
+});
