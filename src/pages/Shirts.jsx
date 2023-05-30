2ec432fe0962138936be5aa1967a7ccfeca16883
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 19:25:33 -0500

    populated the category pages

--- src/pages/Shirts.jsx
+++ src/pages/Shirts.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { productsArray } from '../utility/inventory'
 
 
 function Shirts() {
+  const tempArray= productsArray.filter((product) => {
+    return product.category === "tops"
+  })
+
   return (
-    <div>Shirts</div>
+    <div className='shop-page'>
+      <h1>Shoes</h1>
+      <div className="product-display">
+        {tempArray.map((product, idx) => (
+          <div className="product-card" key={product.id}>
+            <h3>{product.title}</h3>
+            <img src={product.image} alt="product image" />
+            <p>price: {product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
