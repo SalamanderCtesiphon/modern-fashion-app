20cd3a4c57acd2f7e8ece8b8950f2f703a98d4a4
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 20:05:13 -0500

    created a product card component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,39 @@
+import React, { useContext} from 'react'
+import { CartContext } from '../pages/CartContext'
+
+function ProductCard(props) {
+  const product = props.product
+  const cart = useContext(CartContext)
+  const productQuantity = cart.getProductQuantity(product.id)
+
+  return (
+
+    <div className='product-card'>
+      { productQuantity > 0 ?
+        <button onClick={() => cart.deleteFromCart(product.id)} >delete</button>  
+        :
+        <></>
+      }
+      <img src={product.image} alt='product' className='product-image'></img>
+      <h3>{product.title}</h3>
+      <p>${product.price}</p>
+      { productQuantity > 0 ?
+        <>
+          <div className='adjust-item'>
+            <button className='increment' onClick={() => cart.removeOneFromCart(product.id)} >-</button>
+            <div>
+              In cart: {productQuantity}
+            </div>
+            <button className='increment' onClick={() => cart.addOneToCart(product.id)} >+</button>
+          </div>
+        </> 
+        :
+        <>
+         <button className='nav-link' onClick={() => cart.addOneToCart(product.id)} >Add One to Cart</button>
+        </>
+      }
+    </div>
+  )
+}
+
+export default ProductCard
