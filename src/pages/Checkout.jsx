9eeb53d0fa8d7e452882f52b4c22e32f8ccec448
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-29 20:25:34 -0500

    implemented the logic to render the shopping cart to the screen

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from './CartContext'
+import CartProduct from '../components/CartProduct'
+import Icon from '@mdi/react';
+import { mdiArrowLeftBold } from '@mdi/js';
 
-function Checkout() {
+
+
+function CheckOut() {
+  const cart = useContext(CartContext)
+  const productsCount = cart.items.length
+
+  let USDollar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  let cartTotal = cart.getTotalCost().toFixed(2)
+
   return (
-    <div>
-      <h1>Check out mothafuhka</h1>
+    <div className='checkout-page'>
+         {productsCount > 0 ?
+            <>
+              <h3 className='page-head'><a id="link-back" href='/categories'><Icon path={mdiArrowLeftBold} size={1} />Return to shopping</a></h3>
+              {cart.items.map((currentProduct, idx) => (
+                <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
+              ))}
+
+              <h1 className='total'>Total: {USDollar.format(cartTotal)}</h1>
+
+              <div className='buy-btn' onClick={() => alert("we are currently working to implement a payment service")}>
+                Purchase Items!
+              </div>
+            </>
+          :
+          <>
+          <h1>There Are No Items In Your Cart</h1> 
+          <a className="navLinks" href='categories'>Back to Shopping</a>
+          </> 
+          }
     </div>
   )
 }
 
-export default Checkout
+export default CheckOut
