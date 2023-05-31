c1d3d904432c22956bdc0c27a476a1f081c4e310
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-31 13:36:43 -0500

    trying to implement active links styling

--- store/src/components/Header.jsx
+++ store/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { auth } from '../config/firebase'
 import { signOut } from 'firebase/auth'
 import { CartContext } from '../pages/CartContext'
 import logo from '../icons/logo-png.png'
+import { NavLink } from 'react-router-dom'
 
 function Header({currentUser}) {
 
@@ -23,24 +24,26 @@ function Header({currentUser}) {
         <div className="icon-holder"><a href="home"><img src={logo} alt="company logo" id='logo' /></a></div>
       </div>
      <div className="right-side">
-      <div className="right-container">
-          <nav className="link-list">
-            <a className='link-tag' href="shirts">SHIRTS</a>
-            <a className='link-tag' href="pants">PANTS</a>
-            <a className='link-tag' href="shoes">SHOES</a> 
-          </nav>
-        </div>
-        <div className="utility-container">
-          <a className='link-tag' href="checkout">CHECKOUT</a>
-            <div className='shopping-cart'>
-              { productsCount > 0 ? 
-                <div className='product-count'>{productsCount}</div>
-                :
-                <></>
-              }
-            </div>
-          {currentUser ? <button id='log-in-btn' onClick={() => logout()}>Logout</button> : <button><a href='signin'>Sign in</a></button>}
-        </div>
+     <div className="right-container">
+        <nav>
+          <ul className="link-list">
+            <li className="link"><NavLink className='link-tag' to={'/shirts'}>SHIRTS</NavLink></li>
+            <li className="link"><NavLink className='link-tag' to={'pants'}>PANTS</NavLink></li>
+            <li className="link"><NavLink className='link-tag' to={'shoes'}>SHOES</NavLink></li>        
+          </ul>
+        </nav>
+      </div>
+      <div className="utility-container">
+        <a className='link-tag' href="checkout">CHECKOUT</a>
+          <div className='shopping-cart'>
+            { productsCount > 0 ? 
+              <div className='product-count'>{productsCount}</div>
+              :
+              <></>
+            }
+          </div>
+        {currentUser ? <button id='log-in-btn' onClick={() => logout()}>Logout</button> : <button><a href='signin'>Sign in</a></button>}
+      </div>
      </div>
     </div>
   )
