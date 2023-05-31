592c72a0becdca29f29cdf18f5b80f795dcc375b
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-31 11:38:57 -0500

    reorganized the layout of the header grouping elements together in order to place them where i want them

--- store/src/components/Header.jsx
+++ store/src/components/Header.jsx
@@ -21,27 +21,27 @@ function Header({currentUser}) {
     <div className='header'>
       <div className="left-container">
         <div className="icon-holder"><a href="home"><img src={logo} alt="company logo" id='logo' /></a></div>
-        {currentUser ? <h3 className="title">Current User</h3> : <h2 className="title">COFFEE IMPORTS</h2>}
       </div>
+     <div className="right-side">
       <div className="right-container">
-        <ul className="link-list">
-          <li className="link"><a className='link-tag' href="shirts">SHIRTS</a></li>
-          <li className="link"><a className='link-tag' href="pants">PANTS</a></li>
-          <li className="link"><a className='link-tag' href="shoes">SHOES</a></li>
-          <li className="link"><a className='link-tag' href="checkout">CHECKOUT</a>
-          <div className='shopping-cart'>
-            { productsCount > 0 ? 
-              <div className='product-count'>{productsCount}</div>
-              :
-              <></>
-            }
-          </div>
-          </li>
-          
-        </ul>
-        {currentUser ? <button id='log-in-btn' onClick={() => logout()}>Logout</button> : <button><a href='signin'>Sign in</a></button>}
-      
-      </div>
+          <ul className="link-list">
+            <li className="link"><a className='link-tag' href="shirts">SHIRTS</a></li>
+            <li className="link"><a className='link-tag' href="pants">PANTS</a></li>
+            <li className="link"><a className='link-tag' href="shoes">SHOES</a></li>        
+          </ul>
+        </div>
+        <div className="utility-container">
+          <a className='link-tag' href="checkout">CHECKOUT</a>
+            <div className='shopping-cart'>
+              { productsCount > 0 ? 
+                <div className='product-count'>{productsCount}</div>
+                :
+                <></>
+              }
+            </div>
+          {currentUser ? <button id='log-in-btn' onClick={() => logout()}>Logout</button> : <button><a href='signin'>Sign in</a></button>}
+        </div>
+     </div>
     </div>
   )
 }
