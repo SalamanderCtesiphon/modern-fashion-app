2c9583f46f2f2b4e43bdff96e9ee09d4c7b18e9b
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-01 16:56:38 -0500

    created contact page and updated all relevent links

---
+++ store/src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Contact() {
+  
+  return (
+    <div className='home-page'>
+        <h1>Contact</h1>
+    <p className='text-card'>Please don't attempt to contact us. This is not a real company. This is just a portfolio project that I am doing in an attempt to get a job.</p>
+    <br />
+    <p className='text-card'>Now, if your are a recruiter or a human resources manager please do contact me here:<a href='https://www.linkedin.com/in/shannon-brookshire-817577265/' id='social'> @Linked In </a></p>
+   
+    </div>
+  )
+}
+
+export default Contact
