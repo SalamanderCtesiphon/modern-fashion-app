b480af8eb61af1e6d95f08559c2dcad05de07d44
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-02 14:09:05 -0500

    updated the inventory to reflect the price id from stripe payment processors

--- store/src/utility/inventory.js
+++ store/src/utility/inventory.js
@@ -14,35 +14,35 @@ const productsArray = [
     category: 'tops'
   },
   {
-    id: 'price_1NDUryALqsWPo0rqBrjlBt4k',
+    id: 'price_1NEcMrALqsWPo0rqehm08HMs',
     title: 'Jamaican Coffee',
     price: 9.99,
     image: coffee,
     category: 'tops'
   },
   {
-    id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
-    title: 'Guatamalan Antiqua',
+    id: 'price_1NEcNmALqsWPo0rq9vGEKKyi',
+    title: 'Guatemalan Antiqua',
     price: 14.99,
     image: coffee,
     category: 'tops'
   },
   {
-    id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
+    id: 'price_1NEcOpALqsWPo0rqe3TqxJxd',
     title: 'Columbian Coffee',
     price: 10.99,
     image: coffee,
     category: 'tops'
   },
   {
-    id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
+    id: 'price_1NEcZ2ALqsWPo0rqTNRON9Ci',
     title: 'Sumatran Coffee',
     price: 9.99,
     image: coffee,
     category: 'tops'
   },
   {
-    id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
+    id: 'price_1NEcZlALqsWPo0rq4NYNe8fw',
     title: 'Kenyan Coffee',
     price: 19.99,
     image: coffee,
