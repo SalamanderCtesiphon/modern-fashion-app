75925928f95cf2e545770173cc4c62e32158c5a7
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-02 10:20:15 -0500

    renamed the file to shop

---
+++ store/src/pages/Shop.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { productsArray } from '../utility/inventory'
+import ProductCard from '../components/ProductCard'
+
+
+function Shop() {
+  const tempArray= productsArray.filter((product) => {
+    return product.category === "tops"
+  })
+
+  return (
+    <div className='shop-page'>
+      <h1>Shop</h1>
+      <div className="product-display">
+        {tempArray.map((product, idx) => (
+          <div className="product-card" key={product.id}>
+            <ProductCard product={product}></ProductCard>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Shop
