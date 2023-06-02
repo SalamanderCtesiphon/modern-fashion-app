8f5f08b90038a810058b3ab74e3dbf3566f98ef8
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-02 13:00:08 -0500

    updated the inventory to coffee

--- store/src/utility/inventory.js
+++ store/src/utility/inventory.js
@@ -1,7 +1,4 @@
-import tShirts from '../images/md-salman-tWOz2_EK5EQ-unsplash.jpg'
-import shoes from '../images/pexels-andre-moura-2562992.jpg'
-import dressShirt from '../images/nimble-made-N0ke5zChVBU-unsplash(1).jpg'
-
+import coffee from '../images/coffee-beans.jpg'
 
 // coffee: price_1NDUqHALqsWPo0rqaedrsnqV
 // FIRESTORE_API_KEY=sk_test_51NDHdfALqsWPo0rqTYarcWhGze7u5MV3CUfzuJ5XqHacDEVV5FKuMrSjPLv9XfFzroB1DTC6H7mfxVmAKBgA0Dz8006c2RGJAB
@@ -11,23 +8,44 @@ import dressShirt from '../images/nimble-made-N0ke5zChVBU-unsplash(1).jpg'
 const productsArray = [
   {
     id: 'price_1NDUqHALqsWPo0rqaedrsnqV',
-    title: 'Coffee',
+    title: 'Brazilian Coffee',
     price: 4.99, 
-    image: tShirts,
+    image: coffee,
     category: 'tops'
   },
   {
     id: 'price_1NDUryALqsWPo0rqBrjlBt4k',
-    title: 'Sunglasses',
+    title: 'Jamaican Coffee',
     price: 9.99,
-    image: shoes,
+    image: coffee,
     category: 'tops'
   },
   {
     id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
-    title: 'Camera',
-    price: 39.99,
-    image: dressShirt,
+    title: 'Guatamalan Antiqua',
+    price: 14.99,
+    image: coffee,
+    category: 'tops'
+  },
+  {
+    id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
+    title: 'Columbian Coffee',
+    price: 10.99,
+    image: coffee,
+    category: 'tops'
+  },
+  {
+    id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
+    title: 'Sumatran Coffee',
+    price: 9.99,
+    image: coffee,
+    category: 'tops'
+  },
+  {
+    id: 'price_1NDUsuALqsWPo0rqXmagBcXk',
+    title: 'Kenyan Coffee',
+    price: 19.99,
+    image: coffee,
     category: 'tops'
   }
 ]
