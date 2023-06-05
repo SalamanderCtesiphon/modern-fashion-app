2f4c3fe925c172b2eed59c7ce59c4c59223ae2d1
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-05 10:56:19 -0500

    created a mobile menu component

---
+++ store/src/pages/MobileMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MobileMenu() {
+  return (
+    <div className='menu-page'>
+      <a className='menu-link' href="/">Sign In</a>
+      <a className='menu-link' href="shop">Store</a>
+      <a className='menu-link' href="about">About</a>
+      <a className='menu-link' href="contact">Contact</a>
+      <a className='menu-link' href="checkout">Checkout</a>
+    </div>
+  )
+}
+
+export default MobileMenu
